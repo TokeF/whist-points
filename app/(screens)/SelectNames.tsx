be92b6b6ae96c624e85cb6d8e5c799176a1272b9
TokeF <Toke1993@hotmail.com>
2025-03-26 10:44:10 +1100

    enter names styling

--- app/(screens)/SelectNames.tsx
+++ app/(screens)/SelectNames.tsx
@@ -5,6 +5,7 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  StyleSheet,
 } from "react-native";
 import { Link } from "expo-router";
 import GlobalStyles from "../styles/GlobalStyles";
@@ -15,6 +16,11 @@ const EnterNames = () => {
   const handleNameChange = (index: number, name: string) => {
     const newNames = [...playerNames];
     newNames[index] = name;
+    setPlayerNames(newNames);
+  };
+
+  const handleRemoveName = (index: number) => {
+    const newNames = playerNames.filter((_, i) => i !== index);
     setPlayerNames(newNames);
   };
 
@@ -29,13 +35,21 @@ const EnterNames = () => {
     >
       <Text style={{ fontSize: 24, marginBottom: 20 }}>Enter Player Names</Text>
       {playerNames.map((name, index) => (
-        <TextInput
-          key={index}
-          style={GlobalStyles.input}
-          placeholder={`Player ${index + 1}`}
-          value={name}
-          onChangeText={(text) => handleNameChange(index, text)}
-        />
+        <View key={index} style={styles.nameContainer}>
+          <View style={styles.removeButton}></View>
+          <TextInput
+            style={styles.nameInput}
+            placeholder={`Player ${index + 1}`}
+            value={name}
+            onChangeText={(text) => handleNameChange(index, text)}
+          />
+          <TouchableOpacity
+            style={styles.removeButton}
+            onPress={() => handleRemoveName(index)}
+          >
+            <Text style={{ color: "red" }}>-</Text>
+          </TouchableOpacity>
+        </View>
       ))}
       <Link
         href={{
@@ -51,5 +65,21 @@ const EnterNames = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  nameContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  nameInput: {
+    padding: 5,
+    flex: 2,
+  },
+  removeButton: {
+    flex: 1,
+    marginLeft: 10,
+  },
+});
 
 export default EnterNames;
