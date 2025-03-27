49fcd411c4b3f294a2b48b0a114ca7e32a096b8a
TokeF <Toke1993@hotmail.com>
2025-03-27 16:15:40 +1100

    player names are now selectable chips

--- app/components/ScoreBoard.tsx
+++ app/components/ScoreBoard.tsx
@@ -1,20 +1,43 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { useSelector } from "react-redux";
 import { View, Text } from "react-native";
 import { RootState } from "../store/store";
+import { Chip } from "react-native-paper";
 
 const ScoreBoard = () => {
   const players = useSelector((state: RootState) => state.game.players);
+  const [selectedPlayer1, setSelectedPlayer1] = useState("");
+  const [selectedPlayer2, setSelectedPlayer2] = useState("");
+  const [selectedPlayers, setSelectedPlayers] = useState<string[]>([]);
+  const handleToggleChip = async (name: string) => {
+    const updatedChips = selectedPlayers.includes(name)
+      ? selectedPlayers.filter((chip) => chip !== name)
+      : [...selectedPlayers, name];
+    setSelectedPlayers(updatedChips);
+  };
+
+  console.log(selectedPlayer1, selectedPlayer2);
 
   return (
-    <View style={style.container}>
-      {players.map((player, index) => (
-        <View key={index} style={style.playerRow}>
-          <Text style={style.playerName}>{player.name}</Text>
-          <Text style={style.playerScore}>{player.score}</Text>
-        </View>
-      ))}
+    <View>
+      <View style={style.container}>
+        {players.map((player, index) => (
+          <View key={index} style={style.playerRow}>
+            <Chip
+              textStyle={style.playerName}
+              key={index}
+              showSelectedOverlay={true}
+              showSelectedCheck={false}
+              selected={selectedPlayers.includes(player.name)}
+              onPress={() => handleToggleChip(player.name)}
+            >
+              {player.name}
+            </Chip>
+            <Text style={style.playerScore}>{player.score}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
